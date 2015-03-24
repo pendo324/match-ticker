8bf5a0576b941accb1ce842f57e75ba7a44a401a
pendo324 <pendo3247@gmail.com>
2015-03-24 19:04:29 -0400

    rwarwef

--- backend/interface.py
+++ backend/interface.py
@@ -146,8 +146,8 @@ class Interface:
 	def getLeagues(self):
 		league_api = "https://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v0001/?key=" + api_key
 		response = requests.get(league_api, headers={'Accept-Encoding': 'gzip'})
+		print(response.text)
 		if response == 200:
-			print(response.text)
 			with open(os.path.dirname(__file__) + '/../leagues.json', 'w') as json_file:
 			 	json_file.write(json.dumps(items))
 	
