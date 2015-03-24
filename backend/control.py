b0052c10052b641946f77f16470f08a1961b034c
pendo324 <pendo3247@gmail.com>
2015-03-24 16:44:32 -0400

    Finished backend
    Lack of commits = bad. I need more version control. Seriously...

---
+++ backend/control.py
@@ -0,0 +1,19 @@
+import Interface from interface
+
+#Class for running tests
+controller = Interface()
+
+#Update known_teams.json
+controller.processTeams(controller.getLiveGames())
+
+#Update live_games.json
+controller.getLiveGames()
+
+#Update schedule.json
+controller.getSchedules()
+
+#Update leagues.json
+controller.getLeagues()
+
+#Add logos to leagues
+controller.addLeagueLogos()
--- backend/getItemSchema.py
+++ backend/getItemSchema.py
@@ -1,3 +1,17 @@
-http://git.optf2.com/schema-tracking/diff/DOTA%202%20Schema?h=dota2
+from lxml import html
+import requests
+import json
+import os
+
+def getItemSchema():
+	url = 'https://raw.githubusercontent.com/SchemaTracker/SteamEcon/master/cache/schema_570.json'
+	response = requests.get(url, headers={'Accept-Encoding': 'gzip'})
+	if response.status_code == 200:
+		items = response.json()
+		with open(r'item_schema.json', 'w') as json_file:
+		 	json_file.write(json.dumps(items))
+
+if __name__ == '__main__':
+	getItemSchema()
 # <div add> + item_schema.XXX.txt </>
 # current dota2 item schema since the API call is fucked up
