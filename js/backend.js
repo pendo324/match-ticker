96dd2e1712a9525934f39f5e2444b876222ac6ad
pendo324 <pendo3247@gmail.com>
2015-03-26 17:50:36 -0400

    formatting of VS

--- js/backend.js
+++ js/backend.js
@@ -35,7 +35,7 @@ function insertMatch(rad, dire, league_id, players, livein) {
 		console.log(rad_name + ' &&& ' + dire_name);
 		$('#matches').append('<div class=\'row col-xs-10 col-md-10 col-md-offset-1 col-xs-offset-1\' style=\'outline: 1px solid orange;\'><h3 col-md-1> <img src=\'' +
 		tourney_logo + '\' class=\'img-responsive img-thumbnail\' />' + tourney_name + '</h3> <h4 class=\'col-md-6 text-right\'><img src=\'' + 
-		rad_logo + '\' class=\'img-responsive img-thumbnail\' /> ' + rad_name + '<h5><strong> VS </strong></h5></h4>' +
+		rad_logo + '\' class=\'img-responsive img-thumbnail\' /> ' + rad_name + '<strong> VS </strong></h4>' +
 		'</h4><h4 class=\'col-md-6 text-left\'>'+ dire_name +' <img src=\'' + dire_logo + 
 		'\' class=\'img-responsive img-thumbnail\' /></div>');
 	}
