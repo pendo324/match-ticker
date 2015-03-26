b4e7b0e53d27485fb9b64c121f3d794918e27240
pendo324 <pendo3247@gmail.com>
2015-03-26 16:01:23 -0400

    more globals

--- js/backend.js
+++ js/backend.js
@@ -18,15 +18,17 @@ function insertMatch(rad, dire, league_id, players, livein) {
 	//not a future match, so it must be live
 	if (typeof livein === 'undefined') {
 		//get team tag, players, and logo file location
-		console.log(rad);
 		getTeamInfo(rad);
-		console.log(team_name);
+		var rad_name = team_name;
+		var rad_logo = team_logo;
 		getTeamInfo(dire);
+		var dire_name = team_name;
+		var dire_logo = team_logo;
 		getLeagueInfo(league_id);
 		$('#matches').append('<div class=\'row col-xs-10 col-md-10 col-md-offset-1 col-xs-offset-1\'><h1 col-md-1> <img src=\'' +
-		tourney[1] + '\'class=img-\'responsive img-thumbnail\'>' + tourney[0] + '</h1> <div class=\'col-md-5 text-right\'><img src=\'' + 
-		rad_info[1] + '/>\'' + rad_info[0] + '</div> <div class=\'col-md-1 text-center\'> VS </div> <div class=\'col-md-5 text-left\'><img src=\'' + 
-		dire_info[1] + '/>\'' + dire_info[0] + '</div> </div>')	}
+		league_logo + '\'class=img-\'responsive img-thumbnail\'>' + league_name + '</h1> <div class=\'col-md-5 text-right\'><img src=\'' + 
+		team_logo + '/>\'' + rad_name + '</div> <div class=\'col-md-1 text-center\'> VS </div> <div class=\'col-md-5 text-left\'><img src=\'' + 
+		dire_logo + '/>\'' + dire_name + '</div> </div>')	}
 	else { //future match from schedule.json
 		rad_info = getTeamInfo(rad);
 		dire_info = getTeamInfo(dire);
@@ -42,16 +44,12 @@ function getTeamInfo(team_id) {
 		$.each(data.teams, function(i, team) {
 			if (team.id == team_id) {
 				if (team.hasOwnProperty('logo')) {
-					team_name = team.name;
-					team_logo = team.logo;
-					console.log(team.name);
 					teamInfo(team.name, team.logo)
 					return false;
 				}
 				else {
 					logo = '';
-					team_name = team.name;
-					team_logo = team.logo;
+					teamInfo(team.name, team.logo)
 					return false;
 				}
 			}
@@ -70,12 +68,15 @@ function getLeagueInfo(league_id) {
 	var json = $.getJSON('leagues.json', function(data) {
 		$.each(data.result.leagues, function(i, league) {
 			if (league.leagueid == league_id) {
-				return leagueInfo(league.name, league.logo, league.tournament_url);
+				leagueInfo(league.name, league.logo, league.tournament_url);
 			}
 		});
 	});	
 }
 
 function leagueInfo(name, logo, url) {
+	league_name = name;
+	league_logo = logo;
+	league_url = url;
 	return [name, logo, url];
 }
