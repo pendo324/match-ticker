defdc76ff885b628ef44feabd5a96a1e5433b72a
pendo324 <pendo3247@gmail.com>
2015-03-26 01:29:49 -0400

    Lots and lots of changes

--- js/backend.js
+++ js/backend.js
@@ -1,31 +1,55 @@
-var matchList = $.getJson("js/Dota2matches.json").done(function(json) {
-	for (var i = 0; i < json.length; i++) {
-		var match = json.games[i];
-		insertMatch(match["team 1"].name, match["team 2"].name, match["live in"], match["team 1"].bet, match["team 2"].bet)
+function getMatches() {	
+	var matchList = $.getJson("live_games").done(function(json) {
+		for (var i = 0; i < json.length; i++) {
+			var match = json.result.games[i];
+			if (match.hasOwnProperty(radiant_team)) {
+				if (match.hasOwnProperty(dire_team)) {
+					instertMatch(match.radiant_team.team_id, match.dire_team.team_id, match.league_id, match.players);
+				}
+			}
+		}
+	})
+	return matchList;
+}
+
+function insertMatch(rad, dire, league_id, players, livein) {
+	//not a future match, so it must be live
+	if typeof livein === 'undefined' {
+		//get team tag, players, and logo file location
+		rad_info = teamInfo(team_id);
+		dire_info = teamInfo(team_id);
+		tourney = getLeagueInfo();
+		$('#matches').append('<div class=\'row col-xs-10 col-md-10 col-md-offset-1 col-xs-offset-1\'><h1 col-md-1> <img src=\'' +
+		tourney[1] + '\'class=img-\'responsive img-thumbnail\'>' + tourney[0] + '</h1> <div col-md-5>' + rad_info[1] + 
+		rad_info[0] + '</div> <div col-md-1> VS </div> <div col-md-5>' + dire_info[1] + dire_info[0] + '</div> </div>')
 	}
-})
+	else { //future match from schedule.json
 
-function insertMatch(team1, team2, livein, team1bet, team2bet) {
-	var team1Rank = teamRank(team1);
-	var team1Country = teamCountry(team1);
-	var team2Rank = teamRank(team2);
-	var team2Country = teamCountry(team2);
+	}
 }
 
-function teamRank(team) {
-	var teams = $.getJson("js/teams.json").done(function(json) {
-		//search for the team
+function teamInfo(team_id) {
+	var teams = $.getJson("known_teams.json").done(function(json) {
 		for (var i = 0; i < json.length; i++) {
-			return json.teams.world_rank;
+			var team = json.teams[i];
+			if (team.id == team_id) {
+				return [team.name, team.logo, team.tag, team.roster];
+			}
 		}
 	})
 }
 
-function teamCountry(team) {
-	var teams = $.getJson("js/teams.json").done(function(json) {
-		//search for the team
-		for (var i = 0; i < json.length; i++) {
-			return json.teams.country;
+function getLeagueInfo(league_id) {
+	var name = '';
+	var logo = '';
+	var url = '';
+	var logo = $.getJson("leagues.json").done(function(json) {
+		for (var i = 0; i < json.leagues; i++) {
+			var league = json.result.leagues[i];
+			name = league.name;
+			logo = league.logo;
+			url = league.tournament_url;
 		}
 	})
+	return [name, logo, url];
 }
