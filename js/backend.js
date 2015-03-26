ff6beb5672c15050c2f02bfccdb83caa11c7c264
pendo324 <pendo3247@gmail.com>
2015-03-26 13:47:26 -0400

    I think I got it

--- js/backend.js
+++ js/backend.js
@@ -16,9 +16,9 @@ function insertMatch(rad, dire, league_id, players, livein) {
 	//not a future match, so it must be live
 	if (typeof livein === 'undefined') {
 		//get team tag, players, and logo file location
-		rad_info = teamInfo(rad);
-		dire_info = teamInfo(dire);
-		tourney = leagueInfo(league_id);
+		rad_info = getTeamInfo(rad);
+		dire_info = getTeamInfo(dire);
+		tourney = getLeagueInfo(league_id);
 		$('#matches').append('<div class=\'row col-xs-10 col-md-10 col-md-offset-1 col-xs-offset-1\'><h1 col-md-1> <img src=\'' +
 		tourney[1] + '\'class=img-\'responsive img-thumbnail\'>' + tourney[0] + '</h1> <div class=\'col-md-5 text-right\'><img src=\'' + 
 		rad_info[1] + '/>\'' + rad_info[0] + '</div> <div class=\'col-md-1 text-center\'> VS </div> <div class=\'col-md-5 text-left\'><img src=\'' + 
@@ -33,23 +33,15 @@ function insertMatch(rad, dire, league_id, players, livein) {
 }
 
 function getTeamInfo(team_id) {
-	var team_name, team_logo;
 	var json = $.getJSON('known_teams.json', function(data) {
 		$.each(data.teams, function(i, team) {
-			var name, logo;
-			console.log(team_id + ' ' + team.id)
 			if (team.id == team_id) {
-				team_name = team.name;
 				if (team.hasOwnProperty('logo')) {
-					console.log(team.name, team.logo);
-					console.log(name, logo);
-					teamInfo(team.name, team.logo);
+					return teamInfo(team.name, team.logo);
 				}
 				else {
 					logo = '';
-					console.log(team.name, team.logo);
-					console.log(name, logo);
-					teamInfo(team.name, team.logo);
+					return teamInfo(team.name, team.logo);
 				}
 			}
 		});
@@ -64,7 +56,7 @@ function getLeagueInfo(league_id) {
 	var json = $.getJSON('leagues.json', function(data) {
 		$.each(data.result.leagues, function(i, league) {
 			if (league.leagueid == league_id) {
-				leagueInfo(league.name, league.logo, league.tournament_url);
+				return leagueInfo(league.name, league.logo, league.tournament_url);
 			}
 		});
 	});	
